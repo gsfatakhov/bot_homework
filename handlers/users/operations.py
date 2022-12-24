484e25453349ee17b4e30be82dc74571c705ee98
Georgii <fatahoff.georgy@yandex.ru>
2022-12-24 16:43:33 +0300

    change architecture and some misswritings

--- handlers/users/operations.py
+++ handlers/users/operations.py
@@ -5,28 +5,28 @@ from loader import dp, current_commands
 @dp.message_handler(text="/sum")
 async def command_start(message: types.Message):
     current_commands[message.from_user.id] = "sum"
-    await message.answer("Ввидите числа для суммирования")
+    await message.answer("Ввeдите числа для суммирования")
 
 
 @dp.message_handler(text="/sub")
 async def command_start(message: types.Message):
     current_commands[message.from_user.id] = "sub"
-    await message.answer("Ввидите два числа для вычитания второго из первого")
+    await message.answer("Ввeдите два числа для вычитания второго из первого")
 
 
 @dp.message_handler(text="/mul")
 async def command_start(message: types.Message):
     current_commands[message.from_user.id] = "mul"
-    await message.answer("Ввидите числа для умножения")
+    await message.answer("Ввeдите числа для умножения")
 
 
 @dp.message_handler(text="/div")
 async def command_start(message: types.Message):
     current_commands[message.from_user.id] = "div"
-    await message.answer("Ввидите два для деления первого вторым")
+    await message.answer("Ввeдите два для деления первого вторым")
 
 
 @dp.message_handler(text="/evl")
 async def command_start(message: types.Message):
     current_commands[message.from_user.id] = "evl"
-    await message.answer("Ввидите выражение для подсчета")
+    await message.answer("Ввeдите выражение для подсчета")
